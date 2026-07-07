0ba57b460861f7612d6414aa2a2cc218e60b1b4e
Daphne <145420334+DaphneMnlnsn@users.noreply.github.com>
2026-07-07 20:02:02 +0800

    Time: 2 ms (3.69%) | Memory: 47 MB (31.37%) - LeetSync

--- 27-remove-element/remove-element.cs
+++ 27-remove-element/remove-element.cs
@@ -1,16 +1,14 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
+
+        int k = 0;
+
         for(int i = 0; i < nums.Length; i++){
             if(nums[i] == val){
                 nums[i] = int.MaxValue;
             }
-        }
-
-        int k = 0;
-
-        foreach(int count in nums){
-            if(count != int.MaxValue){
-                k ++;
+            else{
+                k++;
             }
         }
 
