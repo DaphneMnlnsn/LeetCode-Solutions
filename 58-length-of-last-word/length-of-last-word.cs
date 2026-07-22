f95c96292323fce049927a3152512af7ba33da75
Daphne <145420334+DaphneMnlnsn@users.noreply.github.com>
2026-07-22 17:27:28 +0800

    Time: 0 ms (100.00%) | Memory: 39.9 MB (67.74%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cs
+++ 58-length-of-last-word/length-of-last-word.cs
@@ -2,11 +2,6 @@ public class Solution {
     public int LengthOfLastWord(string s) {
         string last = s.Split(' ', StringSplitOptions.RemoveEmptyEntries).LastOrDefault();
 
-        int count = 0;
-        foreach(char letter in last){
-            count++;
-        }
-
-        return count;
+        return last.Length;
     }
 }
